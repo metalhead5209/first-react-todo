be1dd3d806c683ec0876a13cb69305e4f11da54c
Aaron Rodi <metalhead5209@gmail.com>
2022-08-14 09:04:53 -0500

    added delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { nanoid } from 'nanoid';
 import "./App.css";
 import FilterButton from "./components/FilterButtons/FilterButton";
 import Form from "./components/Form/Form";
@@ -8,19 +9,39 @@ function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks)
 
+  const toggleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map((task) => {
+      if(id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task
+    })
+    setTasks(updatedTasks)
+  };
+
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  };
+
   const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
 
   const addTask = (name) => {
-    const newTask = { id: 'id', name, completed: false };
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask])
   };
+
+  const tasksNoun = tasks.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${tasks.length} ${tasksNoun} remaining`;
 
   return (
     <>
@@ -31,7 +52,7 @@ function App(props) {
         <div className="filters btn-group stack-exception">
           <FilterButton />
         </div>
-        <h2 id="list-heading">{props.tasks.length} tasks remaining</h2>
+        <h2 id="list-heading">{headingText}</h2>
         <ul
           role="list"
           className="todo-list stack-large stack-exception"
