813a948059672b8d69d14854bddc05666c8fe665
Aaron Rodi <metalhead5209@gmail.com>
2022-08-14 15:59:16 -0500

    adit edit functionality to app

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,16 @@ function App(props) {
     setTasks(updatedTasks)
   };
 
+  const editTask = (id, newName) => {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task
+    });
+    setTasks(editedTaskList);
+  };
+
   const deleteTask = (id) => {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
@@ -32,6 +42,7 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
