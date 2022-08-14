a3b14652dec9e80599f08f2bb29d9585e6d10de9
Aaron Rodi <metalhead5209@gmail.com>
2022-08-13 22:17:47 -0500

    moved todo data into array and mapped each todo into list

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,64 @@ import "./App.css";
 import Todo from "./components/Todo/Todo";
 
 function App(props) {
+  const taskList = props.tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
   return (
     <>
       <div className="todoapp stack-large">
-      <h1>To do? Or not To Do?</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What Should Be Done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <h1>To do? Or not To Do?</h1>
+        <form>
+          <h2 className="label-wrapper">
+            <label htmlFor="new-todo-input" className="label__lg">
+              What Should Be Done?
+            </label>
+          </h2>
+          <input
+            type="text"
+            id="new-todo-input"
+            className="input input__lg"
+            name="text"
+            autoComplete="off"
+          />
+          <button type="submit" className="btn btn__primary btn__lg">
+            Add
+          </button>
+        </form>
+        <div className="filters btn-group stack-exception">
+          <button type="button" className="btn toggle-btn" aria-pressed="true">
+            <span className="visually-hidden">Show </span>
+            <span>all</span>
+            <span className="visually-hidden"> tasks</span>
+          </button>
+          <button type="button" className="btn toggle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show </span>
+            <span>Active</span>
+            <span className="visually-hidden"> tasks</span>
+          </button>
+          <button type="button" className="btn toggle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show </span>
+            <span>Completed</span>
+            <span className="visually-hidden"> tasks</span>
+          </button>
+        </div>
+        <h2 id="list-heading">3 tasks remaining</h2>
+        <ul
+          role="list"
+          className="todo-list stack-large stack-exception"
+          aria-labelledby="list-heading"
+        >
+          {/* <Todo name="eat" completed={true} id='todo-1'/>
+        <Todo name="sleep" completed={false} id='todo-2'/>
+        <Todo name="repeat" completed={false} id='todo-3'/> */}
+          {taskList}
+        </ul>
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        <Todo name="eat" completed={true}/>
-        <Todo name="sleep" completed={false}/>
-        <Todo name="repeat" completed={false}/>
-      </ul>
-    </div>
     </>
   );
 }
